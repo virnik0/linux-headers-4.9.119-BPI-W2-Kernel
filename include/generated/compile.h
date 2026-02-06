862415bed275760e2b370d41663c36ed4a3e0319
virnik0 <virnik@gmail.com>
2026-02-06 10:07:31 +0100

    - enabled hwmon

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#17 SMP PREEMPT Thu Feb 5 16:18:31 CET 2026"
+#define UTS_VERSION "#18 SMP PREEMPT Fri Feb 6 09:58:16 CET 2026"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "BananaPiW2"
 #define LINUX_COMPILER "gcc version 14.2.0 (Debian 14.2.0-19) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.9.119-BPI-W2-Kernel"
+#define UTS_RELEASE "4.9.119-BPI-W2-Kernel+"
